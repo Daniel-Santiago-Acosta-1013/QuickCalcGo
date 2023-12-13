053cb13878ddd226ae57c3a13b3099240741c12d
Daniel-Santiago-Acosta-1013 <santiagoacosta1013@gmail.com>
2023-12-13 12:42:57 -0500

    [add][fix] Add dependency and update expression evaluation logic

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module QuickCalcGo
 
 go 1.17
+
+require github.com/Knetic/govaluate v3.0.0+incompatible // indirect
--- pkg/calculator/calculator.go
+++ pkg/calculator/calculator.go
@@ -1,36 +1,21 @@
 package calculator
 
 import (
-    "fmt"
-    "strconv"
-    "strings"
+    "github.com/Knetic/govaluate"
+    "log"
 )
 
 // Calculate evalúa una expresión matemática y devuelve el resultado.
 func Calculate(expr string) float64 {
-    // Aquí iría la lógica para evaluar la expresión.
-    // Por simplicidad, vamos a dividir la expresión en partes y realizar una operación básica.
-    parts := strings.Fields(expr)
-    if len(parts) != 3 {
-        fmt.Println("Formato de expresión no soportado.")
-        return 0
+    expression, err := govaluate.NewEvaluableExpression(expr)
+    if err != nil {
+        log.Fatalf("Error al crear la expresión: %s", err)
     }
 
-    num1, _ := strconv.ParseFloat(parts[0], 64)
-    operator := parts[1]
-    num2, _ := strconv.ParseFloat(parts[2], 64)
-
-    switch operator {
-    case "+":
-        return num1 + num2
-    case "-":
-        return num1 - num2
-    case "*":
-        return num1 * num2
-    case "/":
-        return num1 / num2
-    default:
-        fmt.Println("Operador desconocido:", operator)
-        return 0
+    result, err := expression.Evaluate(nil)
+    if err != nil {
+        log.Fatalf("Error al evaluar la expresión: %s", err)
     }
+
+    return result.(float64)
 }
